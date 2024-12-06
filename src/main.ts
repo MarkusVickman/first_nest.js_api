1e5a7beb597d327582eb82b3e58e7d7e20c59845
Markus <markus.vickman@hotmail.com>
2024-12-06 13:48:43 +0100

    lagt till cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 //Bootstrap betyder där programmet startar. Vid start startar AppModule och den lyssnar på port 3000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //gör cors tillgängligt
+  app.enableCors();
+
   //Den här är viktig för att lades till för att sköta felhantering och felmeddelande enligt Dto och controller
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
